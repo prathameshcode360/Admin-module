bc184740a57ea9d83776a30dcf37ef5507ce8f13
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2024-12-28 18:49:14 +0530

    created model

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,7 +1,9 @@
 import path from "path";
-
+import ProductModel from "../models/product.model.js";
 export default class ProductController {
   getProducts(req, res) {
+    let products = ProductModel.getProductArray();
+    console.log(products);
     return res.sendFile(
       path.join(path.resolve(), "src", "view", "product.html")
     );
