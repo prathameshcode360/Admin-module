d8711ad43ad066a6a7369736f14ae7fa3e811c17
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2024-12-28 15:06:56 +0530

    created controller

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
+import ProductController from "./src/controllers/product.controller.js";
 
 const server = express();
-server.get("/", (req, res) => {
-  res.send("welcome to node js server");
-});
+
+const controller = new ProductController();
+
+server.get("/", controller.getProducts);
+server.use(express.static("src/view"));
 
 server.listen(3300, () => {
   console.log("server is listening on port 3300");
---
+++ src/controllers/product.controller.js
@@ -0,0 +1,9 @@
+import path from "path";
+
+export default class ProductController {
+  getProducts(req, res) {
+    return res.sendFile(
+      path.join(path.resolve(), "src", "view", "product.html")
+    );
+  }
+}
