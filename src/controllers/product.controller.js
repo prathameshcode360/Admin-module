10181e5608a92cf9b586b77fdbf5091e900cd3b5
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2024-12-31 16:06:38 +0530

    validation for form added

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,5 +1,6 @@
 import path from "path";
 import ProductModel from "../models/product.model.js";
+import { url } from "inspector";
 export default class ProductController {
   getProducts(req, res) {
     let products = ProductModel.getProductArray();
@@ -10,9 +11,27 @@ export default class ProductController {
     // );
   }
   getForm(req, res) {
-    return res.render("newProduct");
+    return res.render("newProduct", { errorMessage: null });
   }
   addNewProduct(req, res) {
+    // validatig data
+    const { name, price, image } = req.body;
+    let errors = [];
+
+    if (!name || name.trim() === "") {
+      errors.push("Name is rquired");
+    }
+    if (!price || Number(price) < 1) {
+      errors.push("Invalid Price");
+    }
+    try {
+      let validImageUrl = new url(image);
+    } catch (err) {
+      errors.push("url is invalid");
+    }
+    if (errors.length > 1) {
+      return res.render("newProduct", { errorMessage: errors[0] });
+    }
     ProductModel.addNew(req.body);
     let products = ProductModel.getProductArray();
     res.render("product", { products });
