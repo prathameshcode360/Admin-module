5a34e8d19c05e518e5365e2a6e7e7a5c316c5156
Prathamesh Mhatugade <prathameshgit2@gmail.com>
2025-01-01 17:44:19 +0530

    validation code implemented with express-validator lib

--- src/midllewares/validation.middleware.js
+++ src/midllewares/validation.middleware.js
@@ -1,16 +1,22 @@
-export default function validatereq(req, res, next) {
-  // validatig data
-  const { name, price } = req.body;
-  let errors = [];
+import { body, validationResult } from "express-validator";
 
-  if (!name || name.trim() === "") {
-    errors.push("Name is rquired");
-  }
-  if (!price || Number(price) < 1) {
-    errors.push("Invalid Price");
-  }
-  if (errors.length > 0) {
-    return res.render("newProduct", { errorMessage: errors[0] });
+export default async function validatereq(req, res, next) {
+  // 1. Setup rules for validation
+  const rules = [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage("Price should be a positive number"),
+    body("image").isURL().withMessage("Invalid URL"),
+  ];
+  // 2. Run those rules
+  await Promise.all(rules.map((rule) => rule.run(req)));
+  // 3. Check for validation errors
+  let validationRes = validationResult(req);
+  if (!validationRes.isEmpty()) {
+    return res.render("newProduct", {
+      errorMessage: validationRes.array()[0].msg,
+    });
   }
   next();
 }
